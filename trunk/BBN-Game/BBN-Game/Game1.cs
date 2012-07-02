50e9f9573135d4a38091060a3afead76a57ef477
bjtal91@gmail.com <bjtal91@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2012-07-02 22:02:10 +0000

    The Actual Game so far:
    Draws the skybox.
    Draws a dummy ship (red fighter)
    Draws the player (blue fighter)
    Has controls (see on screen)

---
+++ trunk/BBN-Game/BBN-Game/Game1.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace BBN_Game
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Objects.playerObject player1;
+        Objects.Destroyer des;
+        Graphics.Skybox.Skybox skyBox;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            player1 = new BBN_Game.Objects.playerObject(this);
+            des = new BBN_Game.Objects.Destroyer(this);
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 1024;
+
+            //graphics.IsFullScreen = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            player1.Initialize();
+            des.Initialize();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            player1.LoadContent(2);
+            des.LoadContent();
+            skyBox = new BBN_Game.Graphics.Skybox.Skybox(this, 1000, new int[] { 1, 1, 1, 1, 1, 1 }, new string[] { "Skybox/SkyboxTop", "Skybox/SkyboxBottom", "Skybox/SkyboxRight", "Skybox/SkyboxLeft", "Skybox/SkyboxFront", "Skybox/SkyboxBack" }); 
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            // TODO: Add your update logic here
+            player1.Update(gameTime);
+            des.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            Camera.CameraMatrices cam = player1.getCamera(gameTime);
+            des.Draw(gameTime, cam);
+            skyBox.Draw(gameTime, cam, player1.Position);
+            player1.Draw(gameTime, cam, spriteBatch, des);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ trunk/BBN-Game/BBN-Game/Objects/ObjectData/ObjectData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#region "XNA Using Statements"
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+/////
+///
+/// Author - Brandon James Talbot
+/// 
+/// This class contains Varibles that the Game Objects require for rotation etc
+////
+
+namespace BBN_Game.Objects.ObjectData
+{
+    class ObjectData
+    {
+        public float pitch;
+        public float yaw;
+        public float roll;
+        public float speed;
+        public float scale;
+        public Vector3 position;
+
+        public ObjectData()
+        {
+            reset();
+        }
+
+
+        public ObjectData(Vector3 Pos, float pitch, float yaw, float roll, float scale)
+        {
+            this.pitch = pitch;
+            this.yaw = yaw;
+            this.roll = roll;
+            this.position = Pos;
+            this.scale = scale;
+        }
+
+        public void reset()
+        {
+            pitch = yaw = roll = speed = 0.0f;
+            scale = 1;
+            position = Vector3.Zero;
+        }
+
+        public void resetAngles()
+        {
+            pitch = yaw = roll = 0.0f;
+        }
+    }
+}
