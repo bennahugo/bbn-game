ae14495d8ebe7a2d9f921badb9ac459f93f8bbc1
bennahugo@gmail.com <bennahugo@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2012-07-03 19:17:30 +0000

    Broken AI

---
+++ AI-Test/BBN-Game/Objects/Object.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBN_Game.Objects
+{
+    class Object
+    {
+        ObjectData.ObjectData Data;
+
+        
+    }
+}
