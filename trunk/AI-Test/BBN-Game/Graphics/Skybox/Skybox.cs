596223005ef070d1cdb018dd7c39573a279e8e2a
bennahugo@gmail.com <bennahugo@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2012-07-04 22:25:59 +0000

    AI-test working navigation

---
+++ trunk/AI-Test/BBN-Game/Graphics/Skybox/Skybox.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+/////
+/// Author - Brandon James Talbot
+///
+/// Draws the skybox for the game by using a Quad Drawer
+/////
+namespace BBN_Game.Graphics.Skybox
+{
+    class Skybox : DrawableGameComponent
+    {
+        /// <summary>
+        /// Global Variables
+        /// 
+        /// Textures [] - array of textures to use
+        /// Quads - the Quads to draw
+        /// </summary>
+        private Texture2D[] textures;
+        private List<QuadDrawer> Quads;
+
+        public Skybox(Game game, int gameRadius, int [] repeat, string[] texNames)
+            : base(game)
+        {
+            Quads = new List<QuadDrawer>();
+            textures = new Texture2D[6];
+            for (int i = 0; i < 6; ++i)
+                textures[i] = Game.Content.Load<Texture2D>(texNames[i]);
+
+            ///// TOP ----------------------------------------------------------------------------------------
+            Quads.Add(new QuadDrawer(new Vector3(gameRadius + 0.5f, gameRadius, -gameRadius),
+                    new Vector3(-gameRadius - 0.5f, gameRadius, -gameRadius),
+                    new Vector3(gameRadius + 0.5f, gameRadius, gameRadius),
+                    new Vector3(-gameRadius - 0.5f, gameRadius, gameRadius), repeat[0], textures[0], Game.GraphicsDevice));
+
+            ///// Bottom ----------------------------------------------------------------------------------------
+            Quads.Add(new QuadDrawer(new Vector3(-gameRadius - 0.5f, -gameRadius, -gameRadius),
+                    new Vector3(gameRadius + 0.5f, -gameRadius, -gameRadius),
+                    new Vector3(-gameRadius - 0.5f, -gameRadius, gameRadius),
+                    new Vector3(gameRadius + 0.5f, -gameRadius, gameRadius), repeat[1], textures[1], Game.GraphicsDevice));
+
+            ///// Right ----------------------------------------------------------------------------------------
+            Quads.Add(new QuadDrawer(new Vector3(gameRadius, gameRadius, -gameRadius - 0.5f),
+                    new Vector3(gameRadius, gameRadius, +gameRadius + 0.5f),
+                    new Vector3(gameRadius, -gameRadius, -gameRadius - 0.5f),
+                    new Vector3(gameRadius, -gameRadius, +gameRadius + 0.5f), repeat[2], textures[2], Game.GraphicsDevice));
+
+            ///// LEFT ----------------------------------------------------------------------------------------
+            Quads.Add(new QuadDrawer(new Vector3(-gameRadius, gameRadius, +gameRadius + 0.5f),
+                    new Vector3(-gameRadius, gameRadius, -gameRadius - 0.5f),
+                    new Vector3(-gameRadius, -gameRadius, +gameRadius + 0.5f),
+                    new Vector3(-gameRadius, -gameRadius, -gameRadius - 0.5f), repeat[3], textures[3], Game.GraphicsDevice));
+
+            ///// FRONT ----------------------------------------------------------------------------------------
+            Quads.Add(new QuadDrawer(new Vector3(gameRadius + 0.5f, gameRadius, gameRadius),
+                    new Vector3(-gameRadius - 0.5f, gameRadius, gameRadius),
+                    new Vector3(gameRadius + 0.5f, -gameRadius, gameRadius),
+                    new Vector3(-gameRadius - 0.5f, -gameRadius, gameRadius), repeat[4], textures[4], Game.GraphicsDevice));
+
+            ///// BACK ----------------------------------------------------------------------------------------
+            Quads.Add(new QuadDrawer(new Vector3(-gameRadius - 0.5f, gameRadius, -gameRadius),
+                    new Vector3(gameRadius + 0.5f, gameRadius, -gameRadius),
+                    new Vector3(-gameRadius - 0.5f, -gameRadius, -gameRadius),
+                    new Vector3(gameRadius + 0.5f, -gameRadius, -gameRadius), repeat[5], textures[5], Game.GraphicsDevice));
+
+            
+        }
+
+        /// <summary>
+        /// Draw method for the entire skybox
+        /// </summary>
+        /// <param name="gt">The game time</param>
+        /// <param name="cam">The camera class</param>
+        /// <param name="playerPos">The players position</param>
+        public void Draw(GameTime gt, Camera.CameraMatrices cam, Vector3 playerPos)
+        {
+            Matrix world = Matrix.CreateTranslation(playerPos);
+            BasicEffect effect = new BasicEffect(Game.GraphicsDevice, new EffectPool());
+            effect.EmissiveColor = new Vector3(1,1,1);
+            foreach (QuadDrawer q in Quads)
+                q.Draw(cam.View, world, cam.Projection, effect); 
+            
+            base.Draw(gt);
+        }
+    }
+}
---
+++ trunk/AI-Test/BBN-Game/Objects/ObjectData/ObjectData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#region "XNA Using Statements"
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+/////
+///
+/// Author - Brandon James Talbot
+/// 
+/// This class contains Varibles that the Game Objects require for rotation etc
+////
+
+namespace BBN_Game.Objects.ObjectData
+{
+    class ObjectData
+    {
+        public float pitch, totalPitch;
+        public float yaw, totalYaw;
+        public float roll, totalRoll;
+        public float speed;
+        public float scale;
+        public Vector3 position;
+
+        public ObjectData()
+        {
+            reset();
+            totalPitch = totalRoll = totalYaw = 0;
+        }
+
+
+        public ObjectData(Vector3 Pos, float pitch, float yaw, float roll, float scale)
+        {
+            this.pitch = pitch;
+            this.yaw = yaw;
+            this.roll = roll;
+            this.position = Pos;
+            this.scale = scale;
+            totalPitch = totalRoll = totalYaw = 0;
+        }
+
+        public void reset()
+        {
+            // reset
+            totalPitch = totalRoll = totalYaw = 0;
+            pitch = yaw = roll = speed = 0.0f;
+            scale = 1;
+            position = Vector3.Zero;
+        }
+
+        public void resetAngles()
+        {
+
+            // Set the totals before resetting
+            totalPitch += pitch;
+            totalRoll += roll;
+            totalYaw += yaw;
+
+            pitch = yaw = roll = 0.0f;
+        }
+    }
+}
