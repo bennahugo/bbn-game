65e629aa4c9acb426c2d6f01afb4a87301aa8b7a
bennahugo@gmail.com <bennahugo@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2012-07-09 14:07:42 +0000

    Updating modified object classes

--- trunk/AI-Test/BBN-Game/AI/NavigationComputer.cs
+++ trunk/AI-Test/BBN-Game/AI/NavigationComputer.cs
@@ -15,18 +15,22 @@ namespace BBN_Game.AI
         private const double EPSILON_DISTANCE = 0.0001f;
         private const int TURN_INTERPOLATION_STEPS = 10;
         private const float TURNING_SPEED_COEF = 1.05f;
-        private Dictionary<DynamicObject, List<Node>> objectPaths;
-        //debug only:
+        private Dictionary<DynamicObject, PathInformation> objectPaths;
+        /// <summary>
+        /// Method to get the current Path of a registered object
+        /// </summary>
+        /// <param name="o">registered object</param>
+        /// <returns>path if it exists. Either an empty list or Null otherwise</returns>
         public List<Node> getPath(DynamicObject o)
         {
-            return objectPaths[o];
+            return objectPaths[o].remainingPath;
         }
         /// <summary>
         /// Default constructor for the class
         /// </summary>
         public NavigationComputer()
         {
-            objectPaths = new Dictionary<DynamicObject, List<Node>>();
+            objectPaths = new Dictionary<DynamicObject, PathInformation>();
         }
         /// <summary>
         /// Sets a new path for an object if it is already registered
@@ -37,7 +41,7 @@ namespace BBN_Game.AI
         public void setNewPathForRegisteredObject(DynamicObject AIObject, Node start, Node end)
         {
             if (objectPaths.Keys.Contains(AIObject))
-                objectPaths[AIObject] = AStar(start, end);
+                objectPaths[AIObject].remainingPath = AStar(start, end);
         }
         /// <summary>
         /// Method to register an object for computer-based navigation
@@ -46,7 +50,7 @@ namespace BBN_Game.AI
         public void registerObject(DynamicObject AIObject)
         {
             if (!objectPaths.Keys.Contains(AIObject))
-                objectPaths.Add(AIObject, null);
+                objectPaths.Add(AIObject, new PathInformation());
         }
         /// <summary>
         /// Method to deregister an object in order to stop computer-based navigation for that object
@@ -61,11 +65,17 @@ namespace BBN_Game.AI
         {
             //TODO: Register collision grid
         }
-        private Boolean SpacialAwarenessTest(DynamicObject ai)
+        private bool SpacialAwarenessTest(DynamicObject ai)
         {
             //TODO: STUB. Test if another object is not to close (otherwise we need to stop the ai for a while)
-            //Return true if ai may move
+            //Return an empty list if ai may move
             return true;
+        }
+        private void avoidCollision(int RecursionDepth, DynamicObject callingAI)
+        {
+            PathInformation pathInfo = objectPaths[callingAI];
+            float lengthOfObjects = 0;
+            callingAI.shipModel.Tag
         }
         /// <summary>
         /// Method to update the movement of all registered AI characters. The list of waypoints have to be in reverse order (as returned by the A*)
@@ -75,76 +85,69 @@ namespace BBN_Game.AI
         {
             foreach (DynamicObject ai in objectPaths.Keys)
             {
-                List<Node> path = objectPaths[ai];
+                PathInformation pathInfo = objectPaths[ai];
                 float closeToWaypoint = ai.getMaxSpeed * DISTANCE_TO_WAYPOINT_IN_SECONDS_WHEN_CLOSE;
                 float veryCloseToWaypoint = ai.getMaxSpeed * DISTANCE_TO_WAYPOINT_IN_SECONDS_WHEN_VERY_CLOSE;
-                if (path != null) //if there is a path
+                if (pathInfo.currentWaypoint != null) //if there is a path
                 {
-                    if (path.Count > 0) //if we have not reached our destination yet
-                    {
-                        Node nextWaypoint = path.Last(); //the last waypoint is the next to move to
-                        float distToWayPoint = (nextWaypoint.Position - ai.Position).Length();
-                        //if very close to the next waypoint remove that waypoint so that we can go to the next:
-                        if (distToWayPoint <= veryCloseToWaypoint)
-                            path.Remove(nextWaypoint);
-                        else
-                        {   //We want our ship to slowly rotate towards the direction it has to move in:
-                            //Calculate yaw and pitch for view direction and target direction
-                            Vector3 vWantDir = Vector3.Normalize(nextWaypoint.Position - ai.Position);
-                            float distance = (float)Math.Sqrt(vWantDir.Z * vWantDir.Z + vWantDir.X * vWantDir.X);
-                            float tpitch = distance == 0 ? (float)Math.Sign(-vWantDir.Y) * (float)Math.PI / 2 : -(float)Math.Atan2(vWantDir.Y, distance);
-                            float tyaw = (float)Math.Atan2(vWantDir.X, vWantDir.Z);
-                            Vector3 vLookDir = Vector3.Normalize(-Matrix.CreateFromQuaternion(ai.rotation).Forward);
-                            distance = (float)Math.Sqrt(vLookDir.Z * vLookDir.Z + vLookDir.X * vLookDir.X);
-                            float cyaw = (float)Math.Atan2(vLookDir.X, vLookDir.Z);
-                            float cpitch = distance == 0 ? (float)Math.Sign(-vLookDir.Y) * (float)Math.PI / 2 : -(float)Math.Atan2(vLookDir.Y, distance);
+                    float distToWayPoint = (pathInfo.currentWaypoint.Position - ai.Position).Length();
+                    List<StaticObject> pathBlockingObjects = SpacialAwarenessTest(ai);
+                    //if very close to the next waypoint remove that waypoint so that we can go to the next:
+                    if (distToWayPoint <= veryCloseToWaypoint)
+                        pathInfo.reachedWaypoint();
+                    else if (pathBlockingObjects.Count == 0)
+                    {   //We want our ship to slowly rotate towards the direction it has to move in:
+                        //Calculate yaw and pitch for view direction and target direction
+                        Vector3 vWantDir = Vector3.Normalize(pathInfo.currentWaypoint.Position - ai.Position);
+                        float distance = (float)Math.Sqrt(vWantDir.Z * vWantDir.Z + vWantDir.X * vWantDir.X);
+                        float tpitch = distance == 0 ? (float)Math.Sign(-vWantDir.Y) * (float)Math.PI / 2 : -(float)Math.Atan2(vWantDir.Y, distance);
+                        float tyaw = (float)Math.Atan2(vWantDir.X, vWantDir.Z);
+                        Vector3 vLookDir = Vector3.Normalize(-Matrix.CreateFromQuaternion(ai.rotation).Forward);
+                        distance = (float)Math.Sqrt(vLookDir.Z * vLookDir.Z + vLookDir.X * vLookDir.X);
+                        float cyaw = (float)Math.Atan2(vLookDir.X, vLookDir.Z);
+                        float cpitch = distance == 0 ? (float)Math.Sign(-vLookDir.Y) * (float)Math.PI / 2 : -(float)Math.Atan2(vLookDir.Y, distance);
 
-                            //now rotate towards the target yaw and pitch
-                            float diffy = tyaw - cyaw;
-                            float diffp = tpitch - cpitch;
+                        //now rotate towards the target yaw and pitch
+                        float diffy = tyaw - cyaw;
+                        float diffp = tpitch - cpitch;
 
-                            //get the direction we need to rotate in:
-                            if (Math.Abs(diffy) > Math.PI)
-                                if (tyaw > cyaw)
-                                    diffy = -(float)(Math.PI * 2 - Math.Abs(diffy));
-                                else
-                                    diffy = (float)(Math.PI * 2 - Math.Abs(diffy));
-
-                            if (Math.Abs(diffp) > Math.PI)
-                                if (tpitch > cpitch)
-                                    diffp = -(float)(Math.PI * 2 - Math.Abs(diffp));
-                                else
-                                    diffp = (float)(Math.PI * 2 - Math.Abs(diffp));
-
-                            if (Math.Abs(diffp) > Math.Abs(ai.getpitchSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds))
-                                diffp = Math.Sign(diffp) * Math.Abs(ai.getpitchSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds);
-                            if (Math.Abs(diffy) > Math.Abs(ai.getYawSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds))
-                                diffy = Math.Sign(diffy) * Math.Abs(ai.getYawSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds);
-                            
-                            Matrix m = Matrix.CreateFromQuaternion(ai.rotation);
-                            Quaternion pitch = Quaternion.CreateFromAxisAngle(m.Right, diffp);    
-                            Quaternion yaw = Quaternion.CreateFromAxisAngle(m.Up, diffy);
-
-                            //special case: deal with the pitch if its PI/2 or -PI/2, because if its slightly off it causes problems:
-                            if (Math.Abs(Math.Abs(tpitch) - Math.PI / 2) <= EPSILON_DISTANCE && !(Math.Abs(diffy) <= EPSILON_DISTANCE))
-                                ai.rotation = Quaternion.CreateFromYawPitchRoll(tyaw, tpitch, 0);
+                        //get the direction we need to rotate in:
+                        if (Math.Abs(diffy) > Math.PI)
+                            if (tyaw > cyaw)
+                                diffy = -(float)(Math.PI * 2 - Math.Abs(diffy));
                             else
-                                ai.rotation = yaw * pitch * ai.rotation;
-                            
-                            //now set the speed:
-                            if (SpacialAwarenessTest(ai))
-                            {
-                                float compLookOntoWant = Vector3.Dot(vLookDir, vWantDir);
-                                if (Math.Abs(compLookOntoWant) > 1)
-                                    compLookOntoWant = 1;
-                                if (distToWayPoint <= closeToWaypoint)
-                                    ai.ShipMovementInfo.speed = ai.getMaxSpeed * (float)(Math.Pow(TURNING_SPEED_COEF, -Math.Abs(Math.Acos(compLookOntoWant) * 180 / Math.PI)));
-                                else
-                                    ai.ShipMovementInfo.speed = ai.getMaxSpeed * (float)(Math.Pow(TURNING_SPEED_COEF, -Math.Abs(Math.Acos(compLookOntoWant) * 180 / Math.PI)));
-                            }
-                        }
+                                diffy = (float)(Math.PI * 2 - Math.Abs(diffy));
+
+                        if (Math.Abs(diffp) > Math.PI)
+                            if (tpitch > cpitch)
+                                diffp = -(float)(Math.PI * 2 - Math.Abs(diffp));
+                            else
+                                diffp = (float)(Math.PI * 2 - Math.Abs(diffp));
+
+                        if (Math.Abs(diffp) > Math.Abs(ai.getpitchSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds))
+                            diffp = Math.Sign(diffp) * Math.Abs(ai.getpitchSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds);
+                        if (Math.Abs(diffy) > Math.Abs(ai.getYawSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds))
+                            diffy = Math.Sign(diffy) * Math.Abs(ai.getYawSpeed) * (float)(gt.ElapsedGameTime.TotalSeconds);
+
+                        Matrix m = Matrix.CreateFromQuaternion(ai.rotation);
+                        Quaternion pitch = Quaternion.CreateFromAxisAngle(m.Right, diffp);
+                        Quaternion yaw = Quaternion.CreateFromAxisAngle(m.Up, diffy);
+
+                        //special case: deal with the pitch if its PI/2 or -PI/2, because if its slightly off it causes problems:
+                        if (Math.Abs(Math.Abs(tpitch) - Math.PI / 2) <= EPSILON_DISTANCE && !(Math.Abs(diffy) <= EPSILON_DISTANCE))
+                            ai.rotation = Quaternion.CreateFromYawPitchRoll(tyaw, tpitch, 0);
+                        else
+                            ai.rotation = yaw * pitch * ai.rotation;
+
+                        //now set the speed:
+                        float compLookOntoWant = Vector3.Dot(vLookDir, vWantDir);
+                        if (Math.Abs(compLookOntoWant) > 1)
+                            compLookOntoWant = 1;
+                        ai.ShipMovementInfo.speed = ai.getMaxSpeed *
+                            (float)(Math.Pow(TURNING_SPEED_COEF, -Math.Abs(Math.Acos(compLookOntoWant) * 180 / Math.PI)));
                     }
-                    else objectPaths[ai].Clear();
+                    else //there are objects in the way
+                        avoidCollision(0, pathBlockingObjects); //calculate a temporary path
                 }
             }
         }
