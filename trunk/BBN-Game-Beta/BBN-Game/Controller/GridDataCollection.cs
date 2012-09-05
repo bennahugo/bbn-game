faebf5854844b053920cfddc33da9ee7c9520473
nathan.floor@gmail.com <nathan.floor@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2012-09-05 11:27:05 +0000

    Beta version of Nebulon 12 (prototype)

---
+++ trunk/BBN-Game-Beta/BBN-Game/Controller/GridDataCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBN_Game.Controller
+{
+    class GridDataCollection
+    {
+        public static void tryCaptureTower(Objects.playerObject player)
+        {
+            List<Grid.GridObjectInterface> list = GameController.Grid.checkNeighbouringBlocks(player);
+
+            foreach (Grid.GridObjectInterface item in list)
+            {
+                if (item is Objects.Turret)
+                {
+                    Objects.Turret turret = ((Objects.Turret)item);
+                    if (turret.Team.Equals(Objects.Team.nutral))
+                    {
+                        turret.changeTeam(player.Team);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
