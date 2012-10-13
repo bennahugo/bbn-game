2ee9c760c4b867567f16e6923e09a1b9eff99876
nathan.floor@gmail.com <nathan.floor@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2012-10-13 14:11:31 +0000

    Completed particle engine and targeting arrows.

---
+++ trunk/BBN-Game-Final/BBN-Game/Controller/GridDataCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBN_Game.Controller
+{
+    class GridDataCollection
+    {
+        public static float MAX_CAPTURE_DISTANCE = 50;
+        public static void tryCaptureTower(Objects.playerObject player)
+        {
+            List<Grid.GridObjectInterface> list = GameController.Grid.checkNeighbouringBlocks(player);
+
+            foreach (Grid.GridObjectInterface item in list)
+            {
+                if (item is Objects.Turret && (item.Position - player.Position).Length() <= MAX_CAPTURE_DISTANCE)
+                {
+                    Objects.Turret turret = ((Objects.Turret)item);
+                    if (turret.Team.Equals(Objects.Team.neutral))
+                    {
+                        turret.changeTeam(player.Team);
+                        GameController.AIController.registerTurretOnTeam(turret, player.Team);
+                        if (player.Team == Objects.Team.Red) 
+                            Controller.GameController.team1.teamCredits += TradingInformation.creditsForCapturingTower;
+                        else
+                            Controller.GameController.team2.teamCredits += TradingInformation.creditsForCapturingTower;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
