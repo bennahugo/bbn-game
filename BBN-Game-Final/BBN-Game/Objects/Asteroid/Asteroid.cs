e43c767cfd055d4efa7fda76468e8176a5189c3b
bjtal91@gmail.com <bjtal91@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2012-10-18 09:22:14 +0000

    asteroid class added

---
+++ BBN-Game-Final/BBN-Game/Objects/Asteroid/Asteroid.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#region "XNA Using Statements"
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace BBN_Game.Objects
+{
+    class Asteroid : StaticObject
+    {
+        public Asteroid(Game game, Team team, Vector3 position) : base(game, team, position)
+        {
+
+        }
+
+        public override void Update(GameTime gt)
+        {
+            this.shipData.roll = (float)(rollSpeed * gt.ElapsedGameTime.TotalSeconds);
+            this.shipData.yaw = (float)(pitchSpeed * gt.ElapsedGameTime.TotalSeconds);
+
+            base.Update(gt);
+        }
+
+        protected override void setData()
+        {
+            Random rand = new Random();
+
+            rollSpeed = (float)rand.NextDouble() * 6 + 3;
+            yawSpeed = (float)rand.NextDouble() * 4 + 3;
+
+            this.mass = 10f;
+            greatestLength = 10.0f;
+            Shield = 100;
+            Health = 100;
+            totalHealth = 100;
+            numHudLines = 4;
+        }
+
+        protected override void resetModels()
+        {
+            model = Game.Content.Load<Model>("Models/Asteroids/AstrFBX");
+
+            base.resetModels();
+        }
+    }
+}
