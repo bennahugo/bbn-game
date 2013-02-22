45105ee4b4ad64df23d987c47a977e59b668ef6e
bennahugo@gmail.com <bennahugo@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2013-02-22 07:41:53 +0000

    Editor unstripped, ported XNA 4.0

---
+++ BBExperiment/BBExperiment/BBExperiment/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BBExperiment")]
+[assembly: AssemblyProduct("BBExperiment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("University of Capetown")]
+[assembly: AssemblyCopyright("Copyright © University of Capetown 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("e00560b8-3058-48ae-bd21-c01bcffd1e76")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
