4c6e913415a5648ec6fe4574321aa6043e77db7b
bjtal91@gmail.com <bjtal91@gmail.com@a1ce0084-011a-343f-1537-f9a305f610df>
2013-03-05 18:42:13 +0000

    brandon experiment added

---
+++ branches/BBExperiment_BETA_Brandon/BBExperiment/BBExperiment/Objects/ExperimentTypes/MovingTarget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#region "XNA Using Statements"
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using BBN_Game.Objects;
+#endregion
+
+
+namespace BBExperiment.Objects
+{
+    class MovingTarget : DynamicObject
+    {
+        public MovingTarget(Game game, Team team, Vector3 position)
+            : base(game, team, position)
+        {
+        }
+    }
+}
